330fa2c9bf708e53ab726e41dacf2106ba502d42
10154507114 <10154507114@ecnu.cn>
2017-12-22 16:29:14 +0800

    ''

--- bin/server.dart
+++ bin/server.dart
@@ -1,9 +1,49 @@
+
 import 'package:redstone/redstone.dart' as app;
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_cors/shelf_cors.dart' as shelf_cors;
+import 'package:sqljocky5/sqljocky.dart';
+import 'package:sqljocky5/utils.dart';
+import 'dart:async';
+import 'dart:convert';
 
-@app.Route("/")
-helloWorld() => "Hello, World!点名程序最棒！";
 
+@app.Route("/data/")
+helloWorld() {
+  return getDataFromDB();
+}
+
+@app.Route("/data/add", methods: const [app.POST])
+addUser(@app.Body(app.TEXT) String userData) {
+  String data = userData;
+  return data;
+}
+
+Future getDataFromDB() async{
+  var pool = new ConnectionPool(
+      host: 'www.muedu.org',port: 3306,
+      user: 'deit-2015',password: 'deit@2015!',
+      db: 'project_2015_example',max: 5);
+      var results = await pool.query('select iduser, firstname,lastname from user');
+      String response;
+      await results.forEach((row) { 
+      response =JSON.encode([ "FirstName: ${row[1]}"," LastName: ${row[2]}"]);
+  });
+      return response;
+     // return '''["林凌","刘冠群","汤夏颖"]''';
+}
+
+@app.Route("/register/")
+register() => "you can now a number";
 main() {
+   Map corsHeaders1 = {
+    "Access-Control-Allow-Methods": "POST",
+    "Access-Control-Allow-Origin": "*",
+  };
+  shelf.Middleware middleware =
+      shelf_cors.createCorsHeadersMiddleware(corsHeaders:corsHeaders1);
   app.setupConsoleLog();
-  app.start();
+  app.addShelfMiddleware(middleware);
+  app.start(port: 90);
 }
