863da601050e0a03b6f21664564f0b90e566446f
10154507114 <10154507114@ecnu.cn>
2017-12-27 21:34:43 +0800

    '随便点击一个座位都能显示学号，并且设定变量为登录成功后的学号'

--- web/main.dart
+++ web/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:html';
 InputElement toDoInput;
+int myid=9;//此变量用来存储登录成功后传递过来的学号；
 var wordList;
 var ab=document.getElementById("startpage");
 var abb=document.getElementById("sidebar");
@@ -10,9 +11,9 @@ var cd=document.getElementById("classpage");
 var de=document.getElementById("lessonpage");
 
 main() async {
-  querySelector('#onlesson-call-char1').onClick.listen(makeRequest);
-  querySelector('#userphoto').onClick.listen(makePostRequest);
-  wordList = querySelector('#studentid1');
+  //querySelector('#onlesson-call-char1').onClick.listen(makeRequest);
+  //querySelector('#userphoto').onClick.listen(makePostRequest);
+ // wordList = querySelector('#studentid9');
   querySelector("#loginbutton")//点击登录按钮，跳转到选课页面
    ..id
    ..onClick.listen(LoginButton);
@@ -25,7 +26,89 @@ main() async {
   querySelector("#randomclass")//左侧点击随机点名
    ..id
    ..onClick.listen(RandomclassButton);
+
+  querySelector('#onlesson-call-char1').onClick.listen(seat1click);
+  querySelector('#onlesson-call-char2').onClick.listen(seat2click);
+  querySelector('#onlesson-call-char3').onClick.listen(seat3click);
+  querySelector('#onlesson-call-char4').onClick.listen(seat4click);
+  querySelector('#onlesson-call-char5').onClick.listen(seat5click);
+  querySelector('#onlesson-call-char6').onClick.listen(seat6click);
+  querySelector('#onlesson-call-char7').onClick.listen(seat7click);
+  querySelector('#onlesson-call-char8').onClick.listen(seat8click);
+  querySelector('#onlesson-call-char9').onClick.listen(seat9click);
+  querySelector('#onlesson-call-char10').onClick.listen(seat10click);
+  querySelector('#onlesson-call-char11').onClick.listen(seat11click);
+  querySelector('#onlesson-call-char12').onClick.listen(seat12click);
+  querySelector('#onlesson-call-char13').onClick.listen(seat13click);
+  querySelector('#onlesson-call-char14').onClick.listen(seat14click);
+  querySelector('#onlesson-call-char15').onClick.listen(seat15click);
+  querySelector('#onlesson-call-char16').onClick.listen(seat16click);
+  querySelector('#onlesson-call-char17').onClick.listen(seat17click);
+  querySelector('#onlesson-call-char18').onClick.listen(seat18click);
+  querySelector('#onlesson-call-char19').onClick.listen(seat19click);
+  querySelector('#onlesson-call-char20').onClick.listen(seat20click);//每个座位设置点击事件，显示相应的学号
 }
+
+void seat1click(Event e){
+  querySelector('#studentid1').text=myid.toString();
+}
+void seat2click(Event e){
+  querySelector('#studentid2').text=myid.toString();
+}
+void seat3click(Event e){
+  querySelector('#studentid3').text=myid.toString();
+}
+void seat4click(Event e){
+  querySelector('#studentid4').text=myid.toString();
+}
+void seat5click(Event e){
+  querySelector('#studentid5').text=myid.toString();
+}
+void seat6click(Event e){
+  querySelector('#studentid6').text=myid.toString();
+}
+void seat7click(Event e){
+  querySelector('#studentid7').text=myid.toString();
+}
+void seat8click(Event e){
+  querySelector('#studentid8').text=myid.toString();
+}
+void seat9click(Event e){
+  querySelector('#studentid9').text=myid.toString();
+}
+void seat10click(Event e){
+  querySelector('#studentid10').text=myid.toString();
+}
+void seat11click(Event e){
+  querySelector('#studentid11').text=myid.toString();
+}
+void seat12click(Event e){
+  querySelector('#studentid12').text=myid.toString();
+}
+void seat13click(Event e){
+  querySelector('#studentid13').text=myid.toString();
+}
+void seat14click(Event e){
+  querySelector('#studentid14').text=myid.toString();
+}
+void seat15click(Event e){
+  querySelector('#studentid15').text=myid.toString();
+}
+void seat16click(Event e){
+  querySelector('#studentid16').text=myid.toString();
+}
+void seat17click(Event e){
+  querySelector('#studentid17').text=myid.toString();
+}
+void seat18click(Event e){
+  querySelector('#studentid18').text=myid.toString();
+}
+void seat19click(Event e){
+  querySelector('#studentid19').text=myid.toString();
+}
+void seat20click(Event e){
+  querySelector('#studentid20').text=myid.toString();
+}//每个位置被点击后显示相应的学号;
 
 void handleError(Object error) {
  wordList.text = 'Request failed.';
