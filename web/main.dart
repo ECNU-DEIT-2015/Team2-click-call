3a4f232f9db2cb23c74314c546d56051f98000cd
10154507114 <10154507114@ecnu.cn>
2018-01-13 10:49:50 +0800

    随堂分组4

--- web/main.dart
+++ web/main.dart
@@ -5,8 +5,6 @@ import 'dart:math' as math;
 InputElement  useridinput1;//此变量用于用户名登陆
 InputElement  userpasswordinput1;
 String myid;//此变量用来存储登录成功后的学号后两位，以便在座位上显示；
-String randomnunow;//将学号后两位转换为字符串；
-UListElement studentlist;//存被点的学生名单;
 var wordList;
 var ab=document.getElementById("startpage");
 var abb=document.getElementById("sidebar");
@@ -71,7 +69,7 @@ querySelector("#recordclass")//左侧点击课堂纪录
   querySelector('#onlesson-call-char20').onClick.listen(seat20click);//每个座位设置点击事件，显示相应的学号
 
  querySelector('#ramdomD').onClick.listen(randomPostRequest);//随机点名事件
- studentlist=querySelector("#namelist");
+ querySelector('#makeT').onClick.listen(maketeamPostRequest);//随机分组事件
 }
 
 void seat1click(Event e){
@@ -191,6 +189,9 @@ Future loginmakePostRequest(Event e) async {
 
 Future randomPostRequest(Event e) async { 
   String url = 'http://localhost:90/data/random';
+  UListElement studentlist;//存被点的学生名单;
+  String randomnunow;//将学号后两位转换为字符串；
+  studentlist=querySelector("#namelist");
   var randomnumber= new List<int>();//存随机点名的学号后两位；
   randomnumber.addAll([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
   InputElement randomstnu = querySelector('#randomst input');
@@ -223,14 +224,42 @@ Future randomPostRequest(Event e) async {
 
 Future maketeamPostRequest(Event e) async { 
   String url = 'http://localhost:90/data/makeTeam';
-  int teamtotal=0;//设置刚开始为0个小组；
   InputElement teamnu = querySelector('#teamnu input');
   int teamnu1=int.parse(teamnu.value);//每组需要的人数，将字符串转换成数字；
+  int teamtotal=0;//设置刚开始为0个小组；
+  var teamst= new List<int>();//每个组分别存在不同的数组里面；
+  teamtotal=20 ~/teamnu1;//将double转化成int，得到一共有几个小组；
+  teamst.length=teamtotal;
+  // teamst.addAll([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
   if(20 % teamnu1==0){
+  int count=1;
+  var flag=1;
+    while(count<=teamnu1){
+      int currentnum=new math.Random().nextInt(20);
+      flag=1;
+      for(int i=1;i<=teamnu1;i++)
+      {
+      if(currentnum==teamst[i]){
+         flag=2;
+         break;}
+      }
+    if(flag==1){
+      teamst[count]=currentnum;
+      randomnunow=currentnum.toString();
+        HttpRequest.request(url, method: 'POST', sendData:randomnunow)
+       .then((HttpRequest resp) {
+      //querySelector('#test').text =resp.responseText;
+      var newnamelist=new LIElement();
+      newnamelist.text=resp.responseText;
+     studentlist.children.add(newnamelist);//将点到的学生名单用列表表示出来;
+       });
+      count++;
+   }  
+  }
     //执行随堂分组
   }
  else {
-  //显示“无法分成均匀的小组，请重新输入小组的人数”;
+ querySelector('#test2').text ="无法分成均匀的小组，请重新输入小组的人数"; //当不能分成均匀小组时
     }
 }//随机分组部分,直接连接数据库
 
